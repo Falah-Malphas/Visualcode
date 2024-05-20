35d89e4bf0c2836df70c6d4c2dbe7f697ec4f241
Falah <alfaridhofalah197@gmail.com>
2024-05-20 12:55:50 +0700

    pembelajaran

---
+++ test.php
@@ -0,0 +1,35 @@
+<?php
+class kucing {
+    public $warna;
+    public $corak;
+    public $warna_mata;
+
+    public function __construct($warna, $corak, $warna_mata){
+        $this->warna = $warna;
+   	    $this->corak = $corak;
+        $this->warna_mata = $warna_mata;
+    }
+    public function meow() { echo “Meow...”; }
+
+    public function makan() { echo “Kucing sedang makan.”; }
+    
+    public function tidur() { echo “Kucing sedang tidur.”; }
+}
+
+$oyen = new Kucing();
+$oyen->warna = “oren”;
+$oyen->corak = “belang”;
+$oyen->warna_mata = “kuning”;
+
+$oyen->meow();
+$oyen->makan();
+$oyen->tidur();
+
+$bleki = new Kucing();
+$bleki->warna = “hitam”;
+$bleki->corak = “polos”;
+$bleki->warna_mata = “kuning”;
+
+$bleki->meow();
+$bleki->makan();
+$bleki->tidur();
