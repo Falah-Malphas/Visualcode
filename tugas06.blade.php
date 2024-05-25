8faed205b7d9ecc41a9f01d474b3a1ead7ffe40d
Falah alfaridho <Alfaridhofalah197@gmail.com>
2024-05-25 21:55:57 +0700

    tugas06

---
+++ tugas06.blade.php
@@ -0,0 +1,24 @@
+<!Doctype html>
+<html>
+    <head>
+        <title>Perkenalan</title>
+    </head>
+    <body>
+        <h1>Selamat datang</h1>
+        Nama: Falah Alfaridho <br>
+        TMP lahir: Jakarta <br>
+        TGL lahir: 05 Januari 2006 <br>
+        <h3>Lulusan</h3>
+        <ol>
+            <li>SDN Tugu 11</li>
+            <li>SMP Negeri 8</li>
+            <li>SMK Harapan Bangsa</li>
+        </ol><br>
+        <h3>Kemampuan<h3>
+        <ol>
+            <li>CSS</li>
+            <li>Javascript</li>
+            <li>PHP</li>
+        </ol>
+    </body>
+</html>
